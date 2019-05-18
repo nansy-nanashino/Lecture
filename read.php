75e63159f3d893e7765696ca787be5136d715ccb
gonbe <rr7wswjc.mt7dcikm@icloud.com>
2019-05-18 14:18:46 +0900

    講義

---
+++ read.php
@@ -0,0 +1,17 @@
+<?php
+
+
+  $filename="data.txt2";
+  if(is_readable($filename)){
+  $handle=fopen($filename,"r");
+  $contents=fread($handle,filesize($filename));
+  fclose($handle);
+  echo $contents;
+ }else{
+  echo "ファイルがありません\n";
+  }
+  //$a = 1;
+  //echo "$a";
+  //echo '$a';
+
+  ?>
