5513a2992a66de585465d17a3ab3ecb29cf6ec7c
gonbe <rr7wswjc.mt7dcikm@icloud.com>
2019-05-18 14:21:58 +0900

    講義

--- write.php
+++ write.php
@@ -1,12 +1,12 @@
 <?php
-  $filename='data.txt';
-  if(is_writable($filename)){
-         $fp = fopen("$filename",'w');
-        fwrite($fp,"aaa\n");
-         fwrite($fp,"bbb\n");
-         fwrite($fp,"ccc\n");
-         fwrite($fp,"ddd\n");
-  }else{
-          "書き込みできません\n";
-  }
+$filename='data.txt';
+if(is_writable($filename)){
+	$fp = fopen("$filename",'w');
+	fwrite($fp,"aaa\n");
+	fwrite($fp,"bbb\n");
+	fwrite($fp,"ccc\n");
+	fwrite($fp,"ddd\n");
+}else{
+	"書き込みできません\n";
+}
 ?>
