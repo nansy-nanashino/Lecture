108e05f4ceaad2ad4a160fd3fb0c04c02761d7f7
gonbe <rr7wswjc.mt7dcikm@icloud.com>
2019-06-24 23:14:10 +0900

    更新

--- input.php
+++ input.php
@@ -1,6 +1,5 @@
 <?PHP
 $filename='data.json';
-
 if(is_readable($filename)){
 	$handle=fopen($filename,'r');
 	$contents=fread($handle,filesize($filename));
@@ -9,54 +8,47 @@ if(is_readable($filename)){
 	//var_dump($object->users[0]->name);
 	//var_dump($array);
 
-	if (isset($_GET["sub"])){
+	if(isset($_GET["sub"])){
 		$btn=$_GET["sub"];
-
 		switch($btn){
-			case "registration":
-				if(is_writable($filename)){
-					$fp = fopen($filename,'w');
-					$array=array();
-					array_push(
-							$array,(object)array(
-								'name'=>$_GET['username'],
-								'birthday'=>$_GET['birthday'],
-								'email'=>$_GET['email']
-								)			
+			case "Registration":if(is_writable($filename)){
+						    $array=array();
+						    $fp = fopen($filename,'w');
+						    //var_dump($array); 
+						    array_push(
+								    $array,(object)array(
+									    'name'=>$_GET['username'],
+									    'birthday'=>$_GET['birthday'],
+									    'email'=>$_GET['email']
+									    )				
 
-						  );
-					//echo $array;
-					//var_dump($array);
-					$json=json_encode($array);
-					fwrite($fp,$json);
-					fclose($fp);
-				}else{
-					echo "書き込みできません\n";
-				};
+							      );
+						    //echo $array;
+						    //var_dump($array);
+						    $json=json_encode($array);
+						    fwrite($fp,$json);
+						    fclose($fp);
+					    }else{
+						    echo "書き込みできません\n";
+					    };
+					    break;
 
-				break;
-			case "alldelete":
-				if(is_writable($filename)){
-					$fp = fopen($filename,'w');
-					$array=array();
+			case  "delete": if(is_writable($filename)){
 
-					//echo $array;
-					//var_dump($array);
-					$json=json_encode($array);
-					fwrite($fp,$json);
-					fclose($fp);
-				}else{
-					echo "書き込みできません\n";
-				};
-				break;
+						$fp = fopen($filename,'w');
+						$array=array();
+						$json=json_encode($array);
+						fwrite($fp,$json);
+						fclose($fp);
+					}else{
+						echo "書き込みできません\n";
+					};
+					break;
 		}
-
-	}	
+	}
 }else{
 	echo "ファイルがありません\n";
-};
-
-
+}
 
 error_log(print_r($array, true));
 ?>
@@ -72,24 +64,21 @@ error_log(print_r($array, true));
 <input name="username" type="textbox"><br>
 <input name="birthday" type="textbox"><br>
 <input name="email" type="textbox"><br>
-<button type="submit" name="sub" value="registration">送信</button>
-<button type="submit" name="sub" value="alldelete">全て削除</button>
+<button type="submit" name="sub" value="Registration">送信</button>
 </form>
 <h1>
 <form action="" method="get">
 <ul>
-<?php foreach ($array as $user) {  ?>
-<li>
-<?php echo $user->name?>
-<br>
-<?php echo $user->birthday?>
-<br>
-<?php echo $user->email ?>
-<button type="submit" name="sub" value="delete">削除</button>
-</li>
-<?php } ?>
-</form>
-</ul>
-</h1>
-</body>
-</html> 
+<?php
+foreach ($array as $user) {  ?>
+	<li><?php echo $user->name?><br>
+		<?php echo $user->birthday?><br>
+		<?php echo $user->email ?></li>
+		<button type="submit" name="sub" value="delete">削除</button>
+		<?php } ?>
+		</form>
+		</ul>
+		</h1>
+		</body>
+		<html> 
+
